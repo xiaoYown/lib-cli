71580853f4c233e7e03b82f393ac5b39781a5569
xiaoYown <qzy09101018@sina.com>
2021-10-14 19:28:48 +0800

    fix: 添加异常打印

--- packages/lib-cli/src/commands/create/create.ts
+++ packages/lib-cli/src/commands/create/create.ts
@@ -3,17 +3,20 @@ import gitly from 'gitly';
 import * as path from 'path';
 import { repository } from '../../constants/resources';
 
-// const download = require('download-git-repo');
-
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 async function create(args: any[]) {
   const dirname = path.resolve(args[0] || `lib-template-${Date.now()}`);
-  // download(`direct:${repository}`, dirname, err => {
-  //   console.log(err ? 'Error' : 'Success');
-  // });
 
-  const res = await gitly(repository, dirname, {});
-  console.log(res);
+  try {
+    const res = await gitly(repository, dirname, {});
+    if (res[0]) {
+      console.info('[LIB CLI] Pulling success');
+    } else {
+      console.error('[LIB CLI] Pulling fail');
+    }
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export default create;
--- packages/lib-cli/src/commands/dev/dev.ts
+++ packages/lib-cli/src/commands/dev/dev.ts
@@ -1,6 +1,6 @@
 import { ENVS, compile } from '@lib-cli/lib-compile';
 
-function dev(..._args: any[]) {
+function dev(..._args: any[]): void {
   compile({
     env: ENVS.DEVELOPMENT,
   });
