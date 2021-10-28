ba302ad1aba89101a60cb987ec3b2da52b78fbfe
xiaoYown <qzy09101018@sina.com>
2021-10-28 14:01:25 +0800

    feat: add select single/tree-shaking

--- packages/lib-cli/src/commands/create/create.ts
+++ packages/lib-cli/src/commands/create/create.ts
@@ -1,5 +1,6 @@
 import * as path from 'path';
 import ora from 'ora';
+import inquirer from 'inquirer';
 import logger from '@lib-cli/logger';
 import {
   branchSingle,
@@ -10,16 +11,37 @@ import { CommandArgs } from '../../typing';
 
 const download = require('download-git-repo');
 
+const choices = [
+  {
+    name: 'single',
+    value: branchSingle,
+  },
+  {
+    name: 'tree-shaking',
+    value: branchTreeShaking,
+  },
+];
+
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-async function create({ args, options }: CommandArgs) {
+async function create({ args }: CommandArgs) {
   const name = args[1] || `lib-template-${Date.now()}`;
   const dirname = path.resolve(name);
-  const branch = options.treeShaking ? branchTreeShaking : branchSingle;
+
+  const answer = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'module',
+      message: 'Select library template:',
+      choices,
+    },
+  ]);
+
   const spinner = ora('Pulling . . .').start();
 
-  download(`${repository}#${branch}`, dirname, err => {
+  download(`${repository}#${answer.module}`, dirname, err => {
     if (err) {
       logger.error(err);
+      spinner.stop();
     } else {
       spinner.succeed('Success.');
     }
